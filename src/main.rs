ea9ab1f4716f517df7b45a2777c2d267d9cae666
Subserial (EVR-00) <me@subserial.website>
2024-12-04 22:47:21 -0800

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 #[allow(dead_code)]
-
 mod puzzles;
 mod today;
 
--- src/puzzles/p05.rs
+++ src/puzzles/p05.rs
@@ -2,13 +2,21 @@ use std::collections::{HashMap, HashSet};
 
 fn parse(data: &str) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
     let (first, second) = data.split_once("\n\n").unwrap();
-    let orders = first.lines().map(|line| {
-        let mut vals = line.split('|').map(|n| n.parse::<u32>().unwrap());
-        (vals.next().unwrap(), vals.next().unwrap())
-    }).collect();
-    let sequences = second.lines().map(|line| {
-        line.split(',').map(|n| n.parse::<u32>().unwrap()).collect::<Vec<_>>()
-    }).collect();
+    let orders = first
+        .lines()
+        .map(|line| {
+            let mut vals = line.split('|').map(|n| n.parse::<u32>().unwrap());
+            (vals.next().unwrap(), vals.next().unwrap())
+        })
+        .collect();
+    let sequences = second
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect();
     (orders, sequences)
 }
 
@@ -37,31 +45,47 @@ fn extract_good(orders: &Vec<(u32, u32)>, sequences: &Vec<Vec<u32>>) -> Vec<usiz
 
 pub fn run_one(data: &str) -> String {
     let (orders, sequences) = parse(data);
-    let good = extract_good(&orders, &sequences).into_iter().collect::<HashSet<_>>();
-    sequences.iter().enumerate().filter(|(i, _)| good.contains(i)).map(|(_, sequence)| sequence[sequence.len() / 2]).sum::<u32>().to_string()
+    let good = extract_good(&orders, &sequences)
+        .into_iter()
+        .collect::<HashSet<_>>();
+    sequences
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| good.contains(i))
+        .map(|(_, sequence)| sequence[sequence.len() / 2])
+        .sum::<u32>()
+        .to_string()
 }
 
 pub fn run_two(data: &str) -> String {
     let (orders, sequences) = parse(data);
-    let good = extract_good(&orders, &sequences).into_iter().collect::<HashSet<_>>();
-    sequences.into_iter().enumerate().filter(|(i, _)| !good.contains(i)).map(| (_, mut sequence)| {
-        let mut ordered = false;
-        while !ordered {
-            ordered = true;
-            let mut seen = HashMap::<u32, usize>::new();
-            for i in 0..sequence.len() {
-                for order in &orders {
-                    if sequence[i] == order.0 {
-                        if let Some(prev_idx) = seen.get(&order.1) {
-                            sequence.swap(i, *prev_idx);
-                            ordered = false;
-                            break
+    let good = extract_good(&orders, &sequences)
+        .into_iter()
+        .collect::<HashSet<_>>();
+    sequences
+        .into_iter()
+        .enumerate()
+        .filter(|(i, _)| !good.contains(i))
+        .map(|(_, mut sequence)| {
+            let mut ordered = false;
+            while !ordered {
+                ordered = true;
+                let mut seen = HashMap::<u32, usize>::new();
+                for i in 0..sequence.len() {
+                    for order in &orders {
+                        if sequence[i] == order.0 {
+                            if let Some(prev_idx) = seen.get(&order.1) {
+                                sequence.swap(i, *prev_idx);
+                                ordered = false;
+                                break;
+                            }
                         }
+                        seen.insert(sequence[i], i);
                     }
-                    seen.insert(sequence[i], i);
                 }
             }
-        }
-        sequence[sequence.len() / 2]
-    }).sum::<u32>().to_string()
+            sequence[sequence.len() / 2]
+        })
+        .sum::<u32>()
+        .to_string()
 }
